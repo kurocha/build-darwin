5cc3b0485d13d6ae9c3bfe3521b27cc9cec47a0b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-06-14 02:27:31 +1200

    Initial darwin build setup.

---
+++ teapot.rb
@@ -0,0 +1,69 @@
+
+#
+#  This file is part of the "Teapot" project, and is released under the MIT license.
+#
+
+teapot_version "1.0.0"
+
+define_target "build-darwin" do |target|
+	target.provides :linker => "Build/darwin"
+	
+	target.provides "Build/darwin" do
+		define Rule, "link.darwin-static-library" do
+			input :object_files, pattern: /\.o/, multiple: true
+			output :library_file, pattern: /\.a/
+			
+			apply do |parameters|
+				run!(
+					environment[:libtool] || "libtool", *environment[:ldflags],
+					"-static", "-o", parameters[:library_file], "-c", *parameters[:object_files]
+				)
+			end
+		end
+		
+		define Rule, "link.darwin-dynamic-library" do
+			input :object_files, pattern: /\.o$/, multiple: true
+			output :library_file, pattern: /\.(dylib)$/
+			
+			apply do |parameters|
+				run!(
+					environment[:libtool] || "libtool",
+					"-dynamic", "-o", parameters[:library_file], *parameters[:object_files],
+					*environment[:ldflags]
+				)
+			end
+		end
+		
+		define Rule, "link.darwin-executable-cpp" do
+			input :object_files, pattern: /\.o$/, multiple: true
+			output :executable_file
+			
+			apply do |parameters|
+				run!(
+					environment[:cxx],
+					environment[:cxxflags],
+					"-o", parameters[:executable_file], parameters[:object_files],
+					environment[:ldflags]
+				)
+			end
+		end
+		
+		define Rule, "build.static-library" do
+			input :source_files
+			
+			parameter :prefix
+			parameter :static_library
+			
+			output :library_file, implicit: true do |arguments|
+				arguments[:prefix] + "lib" + "lib#{arguments[:static_library]}.a"
+			end
+			
+			apply do |parameters|
+				# Make sure the output directory exists:
+				fs.mkpath File.dirname(parameters[:library_file])
+				
+				build source_files: parameters[:source_files], library_file: parameters[:library_file]
+			end
+		end
+	end
+end
