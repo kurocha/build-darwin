5bf45c23d065259b1553eb1a9459b73778757921
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-05 01:32:17 +1300

    Support 2.3 release.

--- teapot.rb
+++ teapot.rb
@@ -3,7 +3,7 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "3.0"
+teapot_version "2.3"
 
 define_target "build-darwin" do |target|
 	target.provides :linker => "Build/darwin"
@@ -51,7 +51,7 @@ define_target "build-darwin" do |target|
 					"-o", parameters[:executable_file].relative_path,
 					*object_files,
 					*environment[:ldflags],
-					"-L" + parameters[:library_path].shortest_path(input_root),
+					# "-L" + parameters[:library_path].shortest_path(input_root),
 					chdir: input_root
 				)
 			end
